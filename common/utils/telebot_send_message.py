20252eec7369d17c045286ff98b0a7bd12b203cc
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2025-09-17 13:09:26 +0300

    code refactoring

--- common/utils/telebot_send_message.py
+++ common/utils/telebot_send_message.py
@@ -1,6 +1,9 @@
 import requests
 
-from telebot_credentials import *
+from telebot_credentials import (
+  BOT_API_TOKEN,
+  PRIVATE_CHAT_ID,
+)
 
 def send_private_telegram_message(message):
   url = f"https://api.telegram.org/bot{BOT_API_TOKEN}/sendMessage?chat_id={PRIVATE_CHAT_ID}&parse_mode=HTML&text={message}"
