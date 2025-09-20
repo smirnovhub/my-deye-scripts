55d54bb610e77744eda391cb0731b3570520a7d7
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2025-09-20 20:58:18 +0300

    move registers to static

--- common/telebot_users.py
+++ common/telebot_users.py
@@ -6,18 +6,16 @@ from telebot_base_users import TelebotBaseUsers
 from deye_registers_factory import DeyeRegistersFactory
 
 class TelebotUsers(TelebotBaseUsers):
-  def __init__(self):
-    self.registers = DeyeRegistersFactory.create_registers()
+  registers = DeyeRegistersFactory.create_registers()
 
   @property
   def allowed_users(self) -> List[TelebotUser]:
     return [
       # ADD AUTHORIZED USERS HERE AND CPECIFY ALLOWED COMMANDS
-      # allowed_commands = TelebotMenuItem.all()
-      # will allow all commands
       TelebotUser(
         name = 'Dimitras Papandopoulos',
         id = 1234567890,
+        # Empty - means all allowed
         allowed_menu_items = [
           # TelebotMenuItem.deye_all_info,
           # TelebotMenuItem.deye_writable_registers,
@@ -26,19 +24,22 @@ class TelebotUsers(TelebotBaseUsers):
           # TelebotMenuItem.deye_slave_info,
           # TelebotMenuItem.deye_all_today_stat,
         ],
+        # Empty - means none disabled (all allowed)
         disabled_menu_items = [
           # TelebotMenuItem.deye_slave_info,
         ],
+        # Empty - means all allowed
         allowed_writable_registers = [
-          # self.registers.ac_couple_frz_high_register,
-          # self.registers.time_of_use_power_register,
-          # self.registers.grid_peak_shaving_power_register,
-          # self.registers.time_of_use_power_register,
-          # self.registers.zero_export_power_register,
+          # TelebotUsers.registers.ac_couple_frz_high_register,
+          # TelebotUsers.registers.time_of_use_power_register,
+          # TelebotUsers.registers.grid_peak_shaving_power_register,
+          # TelebotUsers.registers.time_of_use_power_register,
+          # TelebotUsers.registers.zero_export_power_register,
         ],
+        # Empty - means none disabled (all allowed)
         disabled_writable_registers = [
-          # self.registers.ac_couple_frz_high_register,
-          # self.registers.time_of_use_power_register,
+          # TelebotUsers.registers.ac_couple_frz_high_register,
+          # TelebotUsers.registers.time_of_use_power_register,
         ],
       ),
     ]
@@ -46,8 +47,8 @@ class TelebotUsers(TelebotBaseUsers):
   @property
   def blocked_users(self) -> List[TelebotUser]:
     return [
-      #TelebotUser(
-      #  name = 'Dimitras Papandopoulos',
-      #  id = 1234567890,
-      #),
+      # TelebotUser(
+      #   name = 'Dimitras Papandopoulos',
+      #   id = 1234567890,
+      # ),
     ]
