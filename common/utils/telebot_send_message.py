78e4ddcecd0e191b1fc1f167cbdc7c292db542a6
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2025-09-23 21:04:15 +0300

    handle global exceptions

--- common/utils/telebot_send_message.py
+++ common/utils/telebot_send_message.py
@@ -6,5 +6,8 @@ from telebot_credentials import (
 )
 
 def send_private_telegram_message(message):
-  url = f"https://api.telegram.org/bot{BOT_API_TOKEN}/sendMessage?chat_id={PRIVATE_CHAT_ID}&parse_mode=HTML&text={message}"
-  requests.get(url)
+  try:
+    url = f"https://api.telegram.org/bot{BOT_API_TOKEN}/sendMessage?chat_id={PRIVATE_CHAT_ID}&parse_mode=HTML&text={message}"
+    requests.get(url)
+  except:
+    print(f'Error while sending message to {PRIVATE_CHAT_ID}')
