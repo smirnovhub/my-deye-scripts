97a7dc6f40be9d2ec9bd556fb64741acda5fd702
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2025-09-26 14:01:40 +0300

    handle writable gen port mode

--- deye/src/registers/read_write/gen_port_mode_writable_deye_register.py
+++ deye/src/registers/read_write/gen_port_mode_writable_deye_register.py
@@ -4,7 +4,14 @@ from deye_register_average_type import DeyeRegisterAverageType
 from deye_gen_port_mode import DeyeGenPortMode
 
 class GenPortModeWritableDeyeRegister(IntDeyeRegister):
-  def __init__(self, address: int, name: str, description: str, suffix: str, avg = DeyeRegisterAverageType.none):
+  def __init__(
+    self,
+    address: int,
+    name: str,
+    description: str,
+    suffix: str,
+    avg = DeyeRegisterAverageType.none,
+  ):
     super().__init__(address, name, description, suffix, avg)
     self._value = DeyeGenPortMode.unknown
 
@@ -16,12 +23,26 @@ class GenPortModeWritableDeyeRegister(IntDeyeRegister):
     return DeyeGenPortMode.unknown
 
   def write(self, interactor: DeyeModbusInteractor, value):
-    if not isinstance(value, DeyeGenPortMode):
-      self.error('write(): value should be of type DeyeGenPortMode')
+    register_type = DeyeGenPortMode
+
+    if isinstance(value, str):
+      for mode in register_type:
+        if str(mode) == value:
+          value = mode
+          break
+
+      if not isinstance(value, register_type):
+        self.error(f'write(): icorrect value of {register_type.__name__}')
+
+    if not isinstance(value, register_type):
+      self.error(f'write(): value should be of type {register_type.__name__}')
+
+    if value == register_type.unknown:
+      self.error(f'write(): icorrect value of {register_type.__name__}')
 
     val = value.value
     if val < 0:
-      self.error('write(): value should be > 0')
+      self.error('write(): value should be >= 0')
 
     if interactor.write_register(self.address, [val]) != 1:
       self.error(f'write(): something went wrong while writing {self.description}')
