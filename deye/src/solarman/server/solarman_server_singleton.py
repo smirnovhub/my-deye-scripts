e91809e0b08a70d688a6b2c0190ad6bfbd07a6b3
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2025-09-29 23:03:03 +0300

    remove unused

--- deye/src/solarman/server/solarman_server_singleton.py
+++
@@ -1,9 +0,0 @@
-from typing import Type, Any, Dict
-
-class Singleton(type):
-  _instances: Dict[Type[Any], Any] = {}
-
-  def __call__(cls, *args, **kwargs):
-    if cls not in cls._instances:
-      cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-    return cls._instances[cls]
