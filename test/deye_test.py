13049787cb365e4bdd258b6aea81f4a8e4ced1bf
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2025-09-29 17:24:55 +0300

    add test server

--- test/deye_test.py
+++ test/deye_test.py
@@ -1,4 +1,41 @@
+import os
 import sys
+import time
+import logging
+import subprocess
 
-print('Test stub')
-sys.exit(0)
+from pathlib import Path
+
+current_path = Path(__file__).parent.resolve()
+modules_path = (current_path / '../modules').resolve()
+
+os.chdir(current_path)
+sys.path.append(str(modules_path))
+
+from common_modules import import_dirs
+
+import_dirs(current_path, ['src', '../deye/src', '../common'])
+
+from solarman_server import AioSolarmanServer
+
+server = AioSolarmanServer('127.0.0.1', 8899)
+
+result = subprocess.run(
+  [
+    sys.executable,
+    '../deye/deye',
+    '--get-pv1-power',
+  ],
+  capture_output = True,
+  text = True,
+)
+
+print(f'Output:\n{result.stdout}')
+
+for line in result.stdout.splitlines():
+  if 'pv1_power' in line:
+    print('Line found. Test is ok')
+    sys.exit(0)
+
+print('Line not found. Test failed')
+sys.exit(1)
---
+++ test/deye_test_loggers.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from deye_logger import DeyeLogger
+from deye_base_loggers import DeyeBaseLoggers
+
+class DeyeLoggers(DeyeBaseLoggers):
+  @property
+  def master(self) -> DeyeLogger:
+    return DeyeLogger(
+      name = 'master_inverter',
+      address = '127.0.0.1',
+      serial = 2612749371,
+    )
+
+  @property
+  def slaves(self) -> List[DeyeLogger]:
+    return []
