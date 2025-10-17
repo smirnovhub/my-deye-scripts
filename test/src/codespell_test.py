2377b81feb23b3b5c59b258aebd6f03b00c6c109
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2025-10-17 12:57:38 +0300

    add codespell_test

---
+++ test/src/codespell_test.py
@@ -0,0 +1,46 @@
+import os
+import logging
+import subprocess
+
+from pathlib import Path
+import sys
+
+base_path = '../..'
+current_path = Path(__file__).parent.resolve()
+
+os.chdir(current_path)
+
+logging.basicConfig(
+  level = logging.INFO,
+  format = "[%(asctime)s] [%(levelname)s] %(message)s",
+  datefmt = "%Y-%m-%d %H:%M:%S",
+)
+
+log = logging.getLogger()
+
+def run_test(path: str):
+  commands = [
+    "codespell",
+    os.path.join(base_path, path),
+  ]
+
+  log.info(f'Running codespell for {path}...')
+  log.info(f'Command to execute: {commands}')
+
+  result = subprocess.run(
+    commands,
+    capture_output = True,
+    text = True,
+  )
+
+  output = result.stdout.strip() + '\n' + result.stderr.strip()
+  log.info(f'Command output: {output}')
+
+  if result.returncode != 0:
+    log.info(f'Command returned non-zero exit code: {result.returncode}. Test failed.')
+    sys.exit(1)
+
+run_test('common')
+run_test('deye')
+run_test('telebot')
+run_test('test')
