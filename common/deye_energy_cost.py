72cc9d2713130e8c691c12a9c06b8781593972e6
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2025-10-21 20:35:37 +0300

    update deye_energy_cost

--- common/deye_energy_cost.py
+++ common/deye_energy_cost.py
@@ -1,8 +1,10 @@
+from typing import Dict
+
 class DeyeEnergyCost:
   # To add new price just add current total pv production (in kWh)
   # as a key and add a new price for kWh as value
   @property
-  def pv_energy_costs(self):
+  def pv_energy_costs(self) -> Dict[int, float]:
     return {
       0: 4.32, # apply price 4.32 from start
       # 200.0: 4.5, # apply price 4.5 after 200 kWh
@@ -10,7 +12,23 @@ class DeyeEnergyCost:
     }
 
   @property
-  def gen_energy_costs(self):
+  def grid_purchased_energy_costs(self) -> Dict[int, float]:
+    return {
+      0: 4.32, # apply price 4.32 from start
+      # 200.0: 4.5, # apply price 4.5 after 200 kWh
+      # 300.0: 5.7, # apply price 5.7 after 300 kWh
+    }
+
+  @property
+  def grid_feed_in_energy_costs(self) -> Dict[int, float]:
+    return {
+      0: 4.32, # apply price 4.32 from start
+      # 200.0: 4.5, # apply price 4.5 after 200 kWh
+      # 300.0: 5.7, # apply price 5.7 after 300 kWh
+    }
+
+  @property
+  def gen_energy_costs(self) -> Dict[int, float]:
     return {
       0: 4.32, # apply price 4.32 from start
       # 200.0: 4.5, # apply price 4.5 after 200 kWh
@@ -20,11 +38,3 @@ class DeyeEnergyCost:
   @property
   def currency_code(self) -> str:
     return 'USD'
-
-  @property
-  def current_pv_energy_cost(self) -> float:
-    return list(self.pv_energy_costs.values())[-1]
-
-  @property
-  def current_gen_energy_cost(self) -> float:
-    return list(self.gen_energy_costs.values())[-1]
