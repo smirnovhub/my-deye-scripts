86489b4eea13ebf07cc2b90dec2f661190914db5
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2025-10-24 13:16:42 +0300

    refactoring of cost registers

--- deye/src/registers/read_only/cost/today_energy_cost_register.py
+++ deye/src/registers/read_only/cost/today_energy_cost_register.py
@@ -1,12 +1,11 @@
-from typing import Dict, List
+from typing import Any, Dict
 
 from deye_register import DeyeRegister
-from base_deye_register import BaseDeyeRegister
+from base_energy_cost_register import BaseEnergyCostRegister
 from deye_modbus_interactor import DeyeModbusInteractor
-from deye_energy_cost import DeyeEnergyCost
 from deye_register_average_type import DeyeRegisterAverageType
 
-class TodayEnergyCostRegister(BaseDeyeRegister):
+class TodayEnergyCostRegister(BaseEnergyCostRegister):
   def __init__(
     self,
     energy_register: DeyeRegister,
@@ -15,35 +14,8 @@ class TodayEnergyCostRegister(BaseDeyeRegister):
     description: str,
     avg = DeyeRegisterAverageType.none,
   ):
-    cost = DeyeEnergyCost()
-    self._energy_register = energy_register
-    self._energy_costs = energy_costs
-    super().__init__(0, 0, name, description, cost.currency_code, avg)
-    self._value = 0.0
+    super().__init__(energy_register, energy_costs, name, description, avg)
 
-  @property
-  def addresses(self) -> List[int]:
-    return self._energy_register.addresses
-
-  def enqueue(self, interactor: DeyeModbusInteractor):
-    self._energy_register.enqueue(interactor)
-
-  def read(self, interactors: List[DeyeModbusInteractor]):
-    self._energy_register.read(interactors)
-    return super().read(interactors)
-
-  def read_internal(self, interactor: DeyeModbusInteractor):
+  def read_internal(self, interactor: DeyeModbusInteractor) -> Any:
     energy = self._energy_register.value
     return round(energy * self.current_energy_cost, 2)
-
-  @property
-  def energy_costs(self) -> Dict[int, float]:
-    return self._energy_costs
-
-  @property
-  def current_energy_cost(self) -> float:
-    return list(self._energy_costs.values())[-1]
-
-  @property
-  def energy_register(self) -> DeyeRegister:
-    return self._energy_register
--- deye/src/registers/read_only/cost/total_energy_cost_register.py
+++ deye/src/registers/read_only/cost/total_energy_cost_register.py
@@ -1,11 +1,11 @@
-from typing import Dict
+from typing import Any, Dict
 
 from deye_register import DeyeRegister
 from deye_modbus_interactor import DeyeModbusInteractor
 from deye_register_average_type import DeyeRegisterAverageType
-from today_energy_cost_register import TodayEnergyCostRegister
+from base_energy_cost_register import BaseEnergyCostRegister
 
-class TotalEnergyCostRegister(TodayEnergyCostRegister):
+class TotalEnergyCostRegister(BaseEnergyCostRegister):
   def __init__(
     self,
     energy_register: DeyeRegister,
@@ -16,7 +16,7 @@ class TotalEnergyCostRegister(TodayEnergyCostRegister):
   ):
     super().__init__(energy_register, energy_costs, name, description, avg)
 
-  def read_internal(self, interactor: DeyeModbusInteractor):
+  def read_internal(self, interactor: DeyeModbusInteractor) -> Any:
     total_cost = 0.0
     energy = self.energy_register.value
 
