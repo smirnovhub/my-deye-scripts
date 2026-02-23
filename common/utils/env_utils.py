4fbcd56a75c581105203bd27379422b012f91b33
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2026-02-23 18:02:47 +0200

    add env utils

---
+++ common/utils/env_utils.py
@@ -0,0 +1,25 @@
+import os
+
+class EnvUtils:
+  @staticmethod
+  def is_remote_cache_on() -> bool:
+    value = os.getenv('USE_REMOTE_CACHE', '').strip().lower()
+    return value in ('true', 'yes')
+
+  @staticmethod
+  def turn_remote_cache_on(server: str):
+    os.environ['USE_REMOTE_CACHE'] = 'true'
+    os.environ['REMOTE_CACHE_SERVER'] = server
+
+  @staticmethod
+  def get_remote_cache_server() -> str:
+    return os.getenv('REMOTE_CACHE_SERVER', '').strip()
+
+  @staticmethod
+  def is_tests_on() -> bool:
+    value = os.getenv('IS_TEST_RUN', '').strip().lower()
+    return value in ('true', 'yes')
+
+  @staticmethod
+  def turn_tests_on():
+    os.environ['IS_TEST_RUN'] = 'true'
