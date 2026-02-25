5216aeb263c6bebdc37a37d45201e638e578bbb5
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2026-02-25 15:40:04 +0200

    add execution timeout

--- deyeweb/src/backserver/backserver_config.py
+++ deyeweb/src/backserver/backserver_config.py
@@ -5,10 +5,13 @@ from backserver_env_var import BackServerEnvVar
 class BackServerConfig:
   def __init__(self):
     self.__server_port = BackServerEnvVar("SERVER_PORT", "80", "Local port to listen on")
+    self.__back_execution_timeout = BackServerEnvVar("BACK_EXECUTION_TIMEOUT", "15",
+                                                     "Timeout for back requests execution, s")
     self.__server_host = '0.0.0.0'
 
     self.__all_vars = [
       self.__server_port,
+      self.__back_execution_timeout,
     ]
 
   @property
@@ -19,13 +22,17 @@ class BackServerConfig:
   def SERVER_PORT(self) -> int:
     return self.__server_port.as_int()
 
+  @property
+  def BACK_EXECUTION_TIMEOUT(self) -> float:
+    return self.__back_execution_timeout.as_float()
+
   def print_usage(self):
     print("\nAvailable environment variables:")
     for var in self.__all_vars:
       default_str = f" (default: {var.default})" if var.default else ""
-      print(f"  {var.name:<11} - {var.description}{default_str}")
+      print(f"  {var.name:<22} - {var.description}{default_str}")
     print("")
 
   def print_config(self, logger: logging.Logger):
     for var in self.__all_vars:
-      logger.info(f"{var.name:<11} : {var.value}")
+      logger.info(f"{var.name:<22} : {var.value}")
