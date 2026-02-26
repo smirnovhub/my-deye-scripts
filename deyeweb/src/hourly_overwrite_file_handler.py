5df17d10ef16dc70c574d9f355b3b0d01de774d3
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2026-02-26 22:56:01 +0200

    add lock

--- deyeweb/src/hourly_overwrite_file_handler.py
+++ deyeweb/src/hourly_overwrite_file_handler.py
@@ -1,8 +1,9 @@
 import os
+import time
 import logging
+import threading
 
 from datetime import datetime
-import time # добавлено для проверки даты файла
 
 class HourlyOverwriteFileHandler(logging.Handler):
   def __init__(
@@ -17,6 +18,7 @@ class HourlyOverwriteFileHandler(logging.Handler):
     self.encoding = encoding
     self._current_hour = None
     self._stream = None
+    self._lock = threading.RLock()
 
     os.makedirs(self.directory, exist_ok = True)
     self._open_initial_stream()
@@ -41,36 +43,40 @@ class HourlyOverwriteFileHandler(logging.Handler):
         # File from yesterday - overwrite
         mode = "w"
 
-    self._stream = open(filename, mode = mode, encoding = self.encoding)
-    self._current_hour = hour
+    with self._lock:
+      self._stream = open(filename, mode = mode, encoding = self.encoding)
+      self._current_hour = hour
 
   def _rollover_if_needed(self):
     hour = self._get_hour()
 
     if hour != self._current_hour:
-      if self._stream:
-        self._stream.close()
+      with self._lock:
+        if self._stream:
+          self._stream.close()
 
-      filename = self._build_filename(hour)
+        filename = self._build_filename(hour)
 
-      # Overwrite only on real hour change
-      self._stream = open(filename, mode = "w", encoding = self.encoding)
+        # Overwrite only on real hour change
+        self._stream = open(filename, mode = "w", encoding = self.encoding)
 
-      self._current_hour = hour
+        self._current_hour = hour
 
   def emit(self, record: logging.LogRecord):
     try:
-      self._rollover_if_needed()
+      with self._lock:
+        self._rollover_if_needed()
 
-      msg = self.format(record)
-      self._stream.write(msg + "\n")
-      self._stream.flush()
+        msg = self.format(record)
+        self._stream.write(msg + "\n")
+        self._stream.flush()
 
     except Exception:
       self.handleError(record)
 
   def close(self):
-    if self._stream:
-      self._stream.close()
-      self._stream = None
+    with self._lock:
+      if self._stream:
+        self._stream.close()
+        self._stream = None
     super().close()
