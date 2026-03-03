dbb139eaf238e217fe5f2eb64c0adfe06d134080
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2026-03-03 12:12:10 +0200

    add texts

--- common/blackout/enum/blackout_action.py
+++ common/blackout/enum/blackout_action.py
@@ -1,8 +1,29 @@
-from enum import Enum, auto
+from enum import Enum
 
 class BlackoutAction(Enum):
-  test = auto()
-  empty = auto()
-  stop_charging = auto()
-  switch_to_battery = auto()
-  turn_grid_off = auto()
+  test = 'Test'
+  empty = 'Empty'
+  stop_charging = 'Stop Charging'
+  switch_to_battery = 'Switch To Battery'
+  turn_grid_off = 'Turn Grid Off'
+
+  # Needed to pass mypy checks
+  _title: str
+
+  @property
+  def title(self) -> str:
+    return self._title
+
+  def __new__(cls, title: str):
+    obj = object.__new__(cls)
+    obj._title = title
+    obj._value_ = cls._next_value()
+    return obj
+
+  @classmethod
+  def _next_value(cls) -> int:
+    # Collect all existing _value_ attributes of current enum members
+    existing = [m._value_ for m in cls.__members__.values()]
+    # Return the next integer value: one higher than the current maximum,
+    # or 1 if there are no existing members yet
+    return max(existing, default = -1) + 1
