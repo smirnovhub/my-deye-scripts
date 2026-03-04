cfe56b7fc3802c865e09e7e7debaa16a1859b100
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2026-03-04 12:38:41 +0200

    fix text

--- deyecache/deyecache.py
+++ deyecache/deyecache.py
@@ -183,7 +183,7 @@ async def global_exception_handler(request: Request, exc: Exception):
   return JSONResponse(
     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
     content = {
-      "detail": error_message,
+      "detail": f"deyecache: {error_message}",
       "path": request.url.path,
     },
   )
