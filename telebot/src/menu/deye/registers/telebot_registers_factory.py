8c0d744ab7d1b36053002abc00271da68c32c79f
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2026-03-17 15:55:02 +0200

    code cleanup

--- telebot/src/menu/deye/registers/telebot_registers_factory.py
+++ telebot/src/menu/deye/registers/telebot_registers_factory.py
@@ -1,6 +1,6 @@
 from typing import Optional, Type, TypeVar, Dict, Callable, Any
-from simple_singleton import singleton
 
+from simple_singleton import singleton
 from accumulated_info_registers import AccumulatedInfoRegisters
 from all_settings_registers import AllSettingsRegisters
 from charge_current_registers1 import ChargeCurrentRegisters1
@@ -24,16 +24,22 @@ class TelebotRegistersFactory:
   """
   def __init__(self) -> None:
     self._registry: Dict[Type[Any], Callable[..., Any]] = {}
-    self._register(AccumulatedInfoRegisters)
-    self._register(AllSettingsRegisters)
-    self._register(ChargeCurrentRegisters1)
-    self._register(ChargeCurrentRegisters2)
-    self._register(ForecastRegisters)
-    self._register(MasterInfoRegisters)
-    self._register(MasterSettingsRegisters)
-    self._register(SlaveInfoRegisters)
-    self._register(TodayStatRegisters)
-    self._register(TotalStatRegisters)
+
+    registers = [
+      AccumulatedInfoRegisters,
+      AllSettingsRegisters,
+      ChargeCurrentRegisters1,
+      ChargeCurrentRegisters2,
+      ForecastRegisters,
+      MasterInfoRegisters,
+      MasterSettingsRegisters,
+      SlaveInfoRegisters,
+      TodayStatRegisters,
+      TotalStatRegisters,
+    ]
+
+    for cls in registers:
+      self._register(cls)
 
   def _register(
     self,
