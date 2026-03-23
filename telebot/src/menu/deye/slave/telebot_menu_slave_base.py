6899106ab6e443342b8f3da7c72b162ac94719e9
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2026-03-23 19:20:30 +0200

    remove time sync check from slaves

--- telebot/src/menu/deye/slave/telebot_menu_slave_base.py
+++ telebot/src/menu/deye/slave/telebot_menu_slave_base.py
@@ -3,7 +3,6 @@ import telebot
 
 from typing import List, Type
 
-from telebot_utils import TelebotUtils
 from deye_registers import DeyeRegisters
 from telebot_deye_helper import TelebotDeyeHelper
 from deye_registers_holder import DeyeRegistersHolder
@@ -83,13 +82,6 @@ class TelebotMenuSlaveTotalBase(TelebotMenuItemHandler):
       master_command = self.master_command,
       slave_command = self.slave_command,
     )
-
-    if abs(self.registers.inverter_system_time_diff_register.value
-           ) > TelebotConstants.inverter_system_time_need_sync_difference_sec:
-      # add line break for keyboard
-      choices[TelebotUtils.row_break_str] = TelebotUtils.row_break_str
-      # add time sync command
-      choices[TelebotConstants.sync_inverter_time_button_name] = f'/{TelebotMenuItem.deye_sync_time.command}'
 
     info = TelebotDeyeHelper.get_register_values(holder.all_registers[slave_name].all_registers)
 
