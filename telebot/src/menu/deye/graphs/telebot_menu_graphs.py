9c0b701e86c530d06d2ebef8be9aa4f942e1a510
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2026-03-27 00:59:22 +0200

    test graphs menu

--- telebot/src/menu/deye/graphs/telebot_menu_graphs.py
+++ telebot/src/menu/deye/graphs/telebot_menu_graphs.py
@@ -1,3 +1,5 @@
+from io import BytesIO
+
 import telebot
 import requests
 
@@ -9,10 +11,12 @@ from env_utils import EnvUtils
 from telebot_menu_item import TelebotMenuItem
 from telebot_menu_item_handler import TelebotMenuItemHandler
 from http_session_singleton import HttpSessionSingleton
+from telebot_progress_message import TelebotProgressMessage
 
 class TelebotMenuGraphs(TelebotMenuItemHandler):
   def __init__(self, bot: telebot.TeleBot):
     super().__init__(bot)
+    self._progress = TelebotProgressMessage(bot)
     self._session = HttpSessionSingleton().session
 
   @property
@@ -46,10 +50,27 @@ class TelebotMenuGraphs(TelebotMenuItemHandler):
     try:
       graph_dates = self._get_graph_dates(server_url)
     except Exception as e:
-      self.bot.send_message(str(e))
+      self.bot.send_message(message.chat.id, str(e))
       return
 
-    self.bot.send_message(f"dates = {graph_dates}")
+    self._progress.show(message.chat.id, "Retrieving data")
+
+    try:
+      graph_png = self._get_graph_png(server_url)
+    except Exception as e:
+      self.bot.send_message(message.chat.id, str(e))
+      return
+    finally:
+      self._progress.hide()
+
+    # Use BytesIO to create a file-like object in memory
+    file_data = BytesIO(graph_png)
+    file_data.name = 'image.png'
+
+    # Send the file as a document
+    self.bot.send_document(message.chat.id, file_data)
+
+    #self.bot.send_message(message.chat.id, f"dates = {graph_dates}")
 
   def _is_graph_server_available(self, server_url: str) -> bool:
     ping_endpoint = urljoin(server_url, "/ping")
@@ -62,15 +83,25 @@ class TelebotMenuGraphs(TelebotMenuItemHandler):
     response = self._session.get(url)
 
     if response.status_code != requests.codes.ok:
-      raise RuntimeError(f"Graphs server returned error {response.status_code}: {response.text}")
+      raise RuntimeError(f"Graphs server {server_url} returned error {response.status_code}: {response.text}")
 
     # Check if the response body is valid JSON
     data = response.json()
 
     if not isinstance(data, dict):
-      raise RuntimeError("Graphs server returned wrong type for skip regulation date")
+      raise RuntimeError(f"Graphs server {server_url} returned wrong type for skip regulation date")
 
     if 'dates' not in data:
       raise RuntimeError("No field 'dates' in graphs server response")
 
     return [date.fromisoformat(d) for d in data['dates']]
+
+  def _get_graph_png(self, server_url: str) -> bytes:
+    url = urljoin(server_url, '/graphs/png/2026-03-27/combined/battery_power')
+    response = self._session.get(url)
+
+    if response.status_code != requests.codes.ok:
+      raise RuntimeError(f"Graphs server {server_url} returned error {response.status_code}: {response.text}")
+
+    # Check if the response body is valid JSON
+    return response.content
