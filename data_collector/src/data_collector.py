03f9c01142f8dec023e9362a4d52b746c9409a3d
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2026-04-04 15:46:57 +0300

    add log separator

--- data_collector/src/data_collector.py
+++ data_collector/src/data_collector.py
@@ -86,6 +86,8 @@ async def main_logic(config: DataCollectorConfig, logger: logging.Logger) -> Non
     f.write(thresholds_str)
     f.flush()
 
+  logger.info("-------------------------------------")
+
 async def _read_registers(loggers: DeyeLoggers, logger: logging.Logger) -> DeyeRegistersHolderAsync:
   retry_attempts = 5
   retry_delay_sec = 6
