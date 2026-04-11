1d9a78f4ffdea5e081d891c8f20a24c34f14d8e7
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2026-04-11 23:45:04 +0300

    remove unnecessary files

--- deye_graph_generator/deye_graph_generator_server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-
-set "DEYE_LOG_NAME=deye_graph_generator_server"
-set "REMOTE_GRAPH_SERVER_URL=http://127.0.0.1:1115"
-set "DEYE_PNG_GRAPHS_DIR=deye-png-graphs"
-
-python -u deye_graph_generator_server.py
--- deye_graph_generator/mypy-checks.bat
+++
@@ -1,2 +0,0 @@
-mypy --ignore-missing-imports --python-version 3.8 src
-pause
