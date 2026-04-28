59c4f9b18d82c925ccd824f7d308bd818134f34e
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2026-04-28 11:56:50 +0300

    move to async/await

--- telebot/src/menu/other/telebot_menu_revert.py
+++ telebot/src/menu/other/telebot_menu_revert.py
@@ -4,36 +4,44 @@ import telebot
 from typing import List
 
 from button_node import ButtonNode
-from git_helper import GitHelper
+from git_helper_async import GitHelperAsync
 from common_utils import CommonUtils
 from telebot_utils import TelebotUtils
+from telebot_async_runner import TelebotAsyncRunner
 from telebot_menu_item import TelebotMenuItem
-from telebot_menu_item_handler_sync import TelebotMenuItemHandlerSync
-from telebot_local_update_checker import TelebotLocalUpdateChecker
+from telebot_menu_item_handler_async import TelebotMenuItemHandlerAsync
+from telebot_local_update_checker_async import TelebotLocalUpdateCheckerAsync
 from telebot_constants import TelebotConstants
 from telebot_user_choices import UserChoices
 from telebot_advanced_choice import AdvancedChoice
 from countdown_with_cancel import CountdownWithCancel
 
-class TelebotMenuRevert(TelebotMenuItemHandlerSync):
-  def __init__(self, bot: telebot.TeleBot):
-    super().__init__(bot)
-    self.git_helper = GitHelper()
-    self.update_checker = TelebotLocalUpdateChecker()
+class TelebotMenuRevert(TelebotMenuItemHandlerAsync):
+  def __init__(
+    self,
+    bot: telebot.TeleBot,
+    runner: TelebotAsyncRunner,
+  ):
+    super().__init__(
+      bot = bot,
+      runner = runner,
+    )
+    self._git_helper = GitHelperAsync()
+    self._update_checker = TelebotLocalUpdateCheckerAsync()
 
   @property
   def command(self) -> TelebotMenuItem:
     return TelebotMenuItem.revert
 
-  def process_message(self, message: telebot.types.Message) -> None:
+  async def process_message(self, message: telebot.types.Message) -> None:
     if not self._remote_update_checker.is_on_branch():
       self.bot.send_message(message.chat.id, 'Unable to revert: the repository is not currently on a branch')
       return
 
     try:
-      branch_name = self.git_helper.get_current_branch_name()
-      if not self.git_helper.is_repository_up_to_date():
-        last_commit = self.git_helper.get_last_commit_hash_and_comment()
+      branch_name = await self._git_helper.get_current_branch_name()
+      if not await self._git_helper.is_repository_up_to_date():
+        last_commit = await self._git_helper.get_last_commit_hash_and_comment()
         last_commit = last_commit.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
         self.bot.send_message(
           message.chat.id,
@@ -42,7 +50,7 @@ class TelebotMenuRevert(TelebotMenuItemHandlerSync):
           f"You are currently on branch '{branch_name}':\n<b>{last_commit}</b>",
           parse_mode = "HTML",
         )
-        self.update_checker.check_for_local_updates(self.bot, message.chat.id, force = True)
+        await self._update_checker.check_for_local_updates(self.bot, message.chat.id, force = True)
         return
     except Exception as e:
       self.bot.send_message(message.chat.id, str(e))
@@ -52,11 +60,11 @@ class TelebotMenuRevert(TelebotMenuItemHandlerSync):
     if pos != -1:
       commit_hash = re.sub(r'[^0-9a-f]', '', message.text[pos + 1:])
       if len(commit_hash) >= 32:
-        self.do_revert(message.chat.id, commit_hash)
+        await self._do_revert(message.chat.id, commit_hash)
         return
 
     try:
-      last_commits = self.git_helper.get_last_commits()
+      last_commits = await self._git_helper.get_last_commits()
     except Exception as e:
       self.bot.send_message(message.chat.id, str(e))
       return
@@ -96,30 +104,31 @@ class TelebotMenuRevert(TelebotMenuItemHandlerSync):
     if message.text is not None:
       commit_hash = re.sub(r'[^0-9a-f]', '', message.text)
       if len(commit_hash) >= 32:
-        self.do_revert(message.chat.id, message.text)
+        self.runner.run(self._do_revert(message.chat.id, message.text))
       else:
         self.bot.send_message(message.chat.id, f'Wrong commit hash')
 
-  def do_revert(self, chat_id: int, commit_hash: str):
+  async def _do_revert(self, chat_id: int, commit_hash: str):
     try:
-      self.git_helper.stash_clear()
+      await self._git_helper.stash_clear()
 
-      short_hash = self.git_helper.get_last_commit_short_hash()
+      short_hash = await self._git_helper.get_last_commit_short_hash()
       if commit_hash.startswith(short_hash):
         self.bot.send_message(chat_id, f'You are already on {short_hash}')
         return
 
-      self.git_helper.stash_push()
-      result = self.git_helper.revert_to_revision(commit_hash)
-      self.git_helper.submodule_update()
-      self.git_helper.stash_pop()
+      await self._git_helper.stash_push()
+      result = await self._git_helper.revert_to_revision(commit_hash)
+      await self._git_helper.submodule_update()
+      await self._git_helper.stash_pop()
     except Exception as e:
       self.bot.send_message(chat_id, f'{str(e)}: trying to reset to HEAD and then stash pop again...')
       try:
-        self.git_helper.revert_to_revision('HEAD')
-        self.git_helper.pull()
-        self.git_helper.submodule_update()
-        self.git_helper.stash_pop()
+        await self._git_helper.revert_to_revision('HEAD')
+        await self._git_helper.pull()
+        await self._git_helper.submodule_update()
+        await self._git_helper.stash_pop()
+
         self.bot.send_message(
           chat_id,
           'All changes rolled back to the latest commit. '
