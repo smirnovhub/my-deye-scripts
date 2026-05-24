b3f7feb0d25a3740438f1343799d3a126101addb
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2026-05-24 10:50:36 +0300

    fix DATA_RETENTION_DAYS

--- data_collector/src/data_collector_config.py
+++ data_collector/src/data_collector_config.py
@@ -42,8 +42,8 @@ class DataCollectorConfig:
   @property
   def DATA_RETENTION_DAYS(self) -> int:
     value = self.__data_retention_days.as_int()
-    if not (1 <= value <= 3650):
-      raise ValueError(f"{self.__data_retention_days.name} should be from 1 to 3650 days")
+    if not (3 <= value <= 3650):
+      raise ValueError(f"{self.__data_retention_days.name} should be from 3 to 3650 days")
     return value
 
   @property
