4c03266442065f0e3fc1c55fc4181815e7854040
Dmitry Smirnov <dmitry.smirnoff@gmail.com>
2026-06-10 19:30:22 +0300

    fix grid state graph

--- deyeweb/src/back/configs/deye_web_graphs_config.py
+++ deyeweb/src/back/configs/deye_web_graphs_config.py
@@ -20,6 +20,7 @@ class DeyeWebGraphsConfig:
       registers.gen_power_register,
       registers.grid_external_ct_power_register,
       registers.grid_internal_ct_power_register,
+      registers.grid_state_register,
       registers.grid_voltage_register,
       registers.inverter_ac_temperature_register,
       registers.inverter_dc_temperature_register,
